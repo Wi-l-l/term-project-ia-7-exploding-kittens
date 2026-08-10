2917ff1a80ac026acf7b166ab1baa10ac64c6332
William Liang <williamliang0902@gmail.com>
2026-08-09 21:18:54 -0400

    Sehath's contribution: Bet class

--- TermProject-IA_7/BusinessLogic/Bet.cs
+++ TermProject-IA_7/BusinessLogic/Bet.cs
@@ -2,5 +2,23 @@ namespace TermProject_IA_7.BusinessLogic;
 
 public class Bet
 {
-    
+    private double _currentBet;
+    public double CurrentBet
+    {
+        get => _currentBet;
+        set => _currentBet = value;
+    }
+    public Bet(double initialBet = 0.0)
+    {
+        _currentBet = initialBet;
+    }
+    public void WinBet(ref int coinBalance)
+    {
+        coinBalance += (int)_currentBet;
+    }
+    public void LoseBet(ref int coinBalance)
+    {
+        coinBalance -= (int)_currentBet;
+        if (coinBalance < 0) coinBalance = 0;
+    }
 }
